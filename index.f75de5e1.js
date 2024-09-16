c9f11ce3b58e3043f3fe987fc93b4943ef8f1ca7
Tetyana Chuhai <chugaytatyana@gmail.&git config --global user.email chugaytatyana@gmail.>
2024-09-16 18:21:05 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const message = document.createElement("div");
+    message.classList.add("notification", type);
+    message.style.top = posTop + "px";
+    message.style.right = posRight + "px";
+    const messageTitle = document.createElement("h2");
+    messageTitle.className = "title";
+    messageTitle.innerHTML = title;
+    messageTitle.style.fontSize = "18px";
+    message.append(messageTitle);
+    const messageDescription = document.createElement("p");
+    messageDescription.innerHTML = description.replace("\n", "</br>");
+    message.append(messageDescription);
+    document.body.append(message);
+    setTimeout(()=>message.remove(), 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
